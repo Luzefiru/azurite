cd0f834aa61d01723245a86240e38ca6df74ccde
Luzefiru <ian.deejay.iv@gmail.com>
2023-08-11 22:32:34 +0800

    feat: dynamically resolve HTML page titles based on the first H1 tag

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,52 @@
 const baseURL = '/azurite'; // change this for GitHub pages deployment based on your repository name
 
-/* [[Linked Page]] -> <a href="/linked-page" /> */
+const { cpSync } = require('fs');
+const { join } = require('path');
 const sanitize = require('sanitize-filename');
-const slugifyLink = (pageName) => {
+
+/**
+ * Resolves WikiLinks to slugged anchor tags.
+ *
+ * [[Linked Page]] -> <a href="/linked-page" />
+ */
+function slugifyLink(pageName) {
   pageName = pageName.trim();
   pageName = pageName.split('/').map(sanitize).join('/');
   pageName = pageName.replace(/\s+/g, '-');
   pageName = pageName.toLowerCase();
   return pageName;
-};
+}
 
-const { cpSync } = require('fs');
-const { join } = require('path');
+/**
+ * 11ty custom filter to extract the text content of the first H1 of a given HTML {content}.
+ */
+function extractFirstH1Text(content) {
+  const regex = /<h1>(.*?)<\/h1>/;
+  const match = content.match(regex);
+
+  if (match && match[1]) {
+    return match[1];
+  } else {
+    return 'Untitled Note';
+  }
+}
+
+/**
+ * Executes before building the notes/ directory.
+ */
+function precompileSteps() {
+  cpSync(
+    join(__dirname, 'static/pages/404.html'),
+    join(__dirname, 'notes/404.html'),
+    { force: true }
+  );
+  cpSync(
+    join(__dirname, 'static/pages/index.html'),
+    join(__dirname, 'notes/index.html'),
+    { force: true }
+  );
+}
+
 const wikilinksPlugin = require('markdown-it-wikilinks')({
   baseURL: `${baseURL}/`,
   uriSuffix: '.html',
@@ -23,22 +58,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.amendLibrary('md', (mdLib) => mdLib.use(wikilinksPlugin));
   /* global YAML front matter keys */
   eleventyConfig.addGlobalData('layout', 'layout.njk');
-  eleventyConfig.addGlobalData('baseURL', baseURL);
-  eleventyConfig.addGlobalData('assets', `${baseURL}/static/assets`);
   /* copy files to _site */
   eleventyConfig.addPassthroughCopy('static/assets/**/*');
+  /* custom filters */
+  eleventyConfig.addFilter('extractFirstH1Text', extractFirstH1Text);
   /* copy pages to be parsed to _site */
   eleventyConfig.addWatchTarget('static');
-  cpSync(
-    join(__dirname, 'static/pages/404.html'),
-    join(__dirname, 'notes/404.html'),
-    { force: true }
-  );
-  cpSync(
-    join(__dirname, 'static/pages/index.html'),
-    join(__dirname, 'notes/index.html'),
-    { force: true }
-  );
+  precompileSteps();
   return {
     dir: {
       input: 'notes',
